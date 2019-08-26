6d215762ef396f4c07140afcc3132069040f0796
Ali <sali40@ms.ds.uhc.com>
2019-08-26 15:51:12 +0530

    add app insight sdk

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+const appInsights = require("applicationinsights");
+appInsights.setup("9c9a6df0-c952-4c4d-bf57-908177a7268c");
+appInsights.start();
 const {  dialogflow  } = require('actions-on-google');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = dialogflow({debug:true});
 
-app.intent('Default Welcome Intent', conv => {
+app.intent('Default Welcome Intecd..Default Fallback Intentnt', conv => {
   conv.ask('Hello Judith, Welcome to Appointment Checker. You can say things such as Make an appointment or Check an appointment.')
 });
 
