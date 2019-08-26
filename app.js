d1ed9ec897bb06941a8c9e883dcfddec18343441
Ali <sali40@ms.ds.uhc.com>
2019-08-26 15:18:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const {  dialogflow  } = require('actions-on-google');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = dialogflow({debug:true});
+
+app.intent('Default Welcome Intent', conv => {
+  conv.ask('Hello Judith, Welcome to Appointment Checker. You can say things such as Make an appointment or Check an appointment.')
+});
+
+app.intent('Default Fallback Intent', conv => {
+  conv.ask(`I didn't understand. Can you tell me something else?`)
+});
+  
+const expressApp = express().use(bodyParser.json())
+expressApp.post('/fulfillment', app)
+var port=process.env.PORT||3000;
+expressApp.listen(port);
+
